95227d259532e0e72cf092f10909c1ddca8e8363
Mirza Abdul Awal <awal.mirza2016@gmail.com>
2021-09-16 18:03:01 -0400

    updated files with new changes!

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 DROP DATABASE IF EXISTS musical-world_db;
 CREATE DATABASE musical-world_db;
-=======
-DROP DATABASE IF EXISTS musicworld_db;
-
-CREATE DATABASE musicworld_db;
->>>>>>> 77825f4b7f52cdc8c392d9c7b92f598c491dd59f
